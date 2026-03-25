09bd9cf40910d6f8fb3f3451fa8e8dd79ba09e07
Alev03YT <55165974+Alev03YT@users.noreply.github.com>
2026-03-25 19:36:25 +0100

    Create firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "INSERISCI_API_KEY",
+  authDomain: "INSERISCI_AUTH_DOMAIN",
+  projectId: "INSERISCI_PROJECT_ID",
+  storageBucket: "INSERISCI_STORAGE_BUCKET",
+  messagingSenderId: "INSERISCI_MESSAGING_SENDER_ID",
+  appId: "INSERISCI_APP_ID"
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+
+export default app
